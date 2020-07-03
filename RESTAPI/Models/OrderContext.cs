f2edf8719147b024c33d28348b325aa03afd9119
Dat Loi <datloi95@gmail.com>
2020-07-02 23:27:02 -0400

    backend skeleton code

---
+++ RESTAPI/Models/OrderContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RESTAPI.Models
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options): base(options)
+        {
+        }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
